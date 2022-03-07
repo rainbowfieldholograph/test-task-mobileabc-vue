9f242201eb9d82725685036fb1fe2297dbb6ee22
rainbowfieldholograph <somebleed@tutanota.com>
2022-03-07 22:10:17 +0900

    changed router

--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,14 @@
 import Home from '../pages/Home.vue';
 import Quiz from '../pages/Quiz.vue';
 import Result from '../pages/Result.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/quiz', component: Quiz },
   { path: '/result', component: Result },
 ];
-const history = createWebHistory();
+const history = createWebHashHistory();
 
 const router = createRouter({ routes, history });
 
